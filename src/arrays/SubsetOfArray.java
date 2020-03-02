3e83e26044161384737e2c2a7e7a2fce8cb91e15
Aman Verma <amanvermaa29@gmail.com>
2020-03-03 02:36:29 +0530

    subset iterative approach

--- src/arrays/SubsetOfArray.java
+++ src/arrays/SubsetOfArray.java
@@ -2,18 +2,38 @@ package arrays;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class SubsetOfArray {
 
 	public static void main(String[] args) {
-		
-			int result[][] = subsets(new int[]{1,2,3});
+			int result[][] = subsets(new int[]{2,1,3});
+			Arrays.sort(result, new Comparator<int[]>() {
+				@Override
+				public int compare(int[] a, int[] b) {
+			         int an = a.length;
+		                int bn = b.length;
+		                for (int i = 0; i < Math.min(an, bn); i++) {
+		                    int cmp = Integer.compare(a[i], b[i]);
+		                    if (cmp != 0)
+		                        return cmp;
+		                }
+		                return Integer.compare(a.length, b.length);
+					
+					
+					
+				/*
+				 * int comp =0; if(o2.length !=0) { comp=(o1[0] < o2[0]) ? -1 : ((o1[0] ==
+				 * o2[0]) ? 0 : 1); } return comp;
+				 */
+				}			
+			});
 			for (int[] is : result) {
 				for(int j : is) {
-					 System.out.println(j);
+					 System.out.print("["+j+"]");
 			}
-				System.out.println();
+				System.out.print(",");
 	}
 	}
 	public static int[][] subsets(int[] A){
